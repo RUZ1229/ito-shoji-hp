c7cb3848da9814c1c8704998eb17f40c3c058b61
ito rui <ruz@itonoMacBook-Air.local>
2026-09-08 14:53:26 +0900

    chiba-map shain+tasha 2026-09-08T05:53:26Z

--- chiba-map/js/chiba-map.js
+++ chiba-map/js/chiba-map.js
@@ -272,7 +272,7 @@
   }
 
   function defaultStatusText() {
-    return `現場 ${mapData.site_count}件 / 更新 ${formatGenerated(mapData.generated)}`;
+    return `現場 ${mapData.site_count}件 / 更新 ${formatGenerated(mapData.generated)} 【反映テスト1455】`;
   }
 
   function formatVisitLine(site) {
